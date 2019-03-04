ff6f2809c2328962f9fbbf7b4969f539d9feedfc
盛领宜 <sheng.ly_wh@www.sand.com>
2019-03-04 09:49:02 +0800

    初始提交

---
+++ src/main/java/com/sly/demo/sensitiveword/DemoSensitivewordApplication.java
@@ -0,0 +1,13 @@
+package com.sly.demo.sensitiveword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSensitivewordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSensitivewordApplication.class, args);
+	}
+
+}
